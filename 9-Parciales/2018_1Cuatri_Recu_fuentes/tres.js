4ee7a10e2c9b267e366261f5b42e017f58b806e3
S-Collazo <santiago_collazo@hotmail.com>
2022-07-07 20:14:19 -0300

    commit

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,20 @@
 function mostrar()
 {
+let precio;
+let porcentaje;
+let descuento;
+let precioFinal;
 
+precio = prompt("Precio: ");
+precio = parseFloat(precio);
+
+porcentaje = prompt("Porcentaje: ");
+porcentaje = parseFloat(porcentaje);
+
+descuento = precio * porcentaje / 100;
+
+precioFinal = precio - descuento;
+precioFinal = precioFinal.toFixed(2);
+
+document.getElementById('elPrecioFinal').value = precioFinal;
 }
