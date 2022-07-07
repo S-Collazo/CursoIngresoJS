c938c2011bed52a604f0fd09053a2e2c62afb42c
S-Collazo <santiago_collazo@hotmail.com>
2022-07-06 22:10:49 -0300

    commit

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -4,15 +4,50 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+function Rectangulo() {
+    let largo;
+    let ancho;
+    let perimetro;
+    let rectangulo;
 
+    largo = document.getElementById('txtIdLargo').value;
+    largo = parseFloat(largo);
+
+    ancho = document.getElementById('txtIdAncho').value;
+    ancho = parseFloat(ancho);
+
+    perimetro = largo * 2 + ancho * 2;
+    rectangulo = perimetro * 3;
+
+    alert("Largo de alambre: " + rectangulo.toFixed(2) + " cm");
 }
-function Circulo () 
-{
-	
+function Circulo() {
+    let radio;
+    let circunferencia;
+    let circulo;
+
+    radio = document.getElementById('txtIdRadio').value;
+    radio = parseFloat(radio);
+
+    circunferencia = 2 * Math.PI * radio;
+    circulo = circunferencia * 3;
+
+    alert("Largo de alambre: " + circulo.toFixed(2) + " cm");
 }
-function Materiales () 
-{
-	
+function Materiales() {
+    let largo;
+    let ancho;
+    let perimetro;
+    let rectangulo;
+
+    largo = document.getElementById('txtIdLargo').value;
+    largo = parseFloat(largo);
+
+    ancho = document.getElementById('txtIdAncho').value;
+    ancho = parseFloat(ancho);
+
+    perimetro = largo * 2 + ancho * 2;
+    rectangulo = perimetro * 3;
+
+    alert("Largo del alambre: " + rectangulo.toFixed(2) + " cm");
 }
