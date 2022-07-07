70ec9079178b74e19766d3b726ec0056f8e4da86
S-Collazo <santiago_collazo@hotmail.com>
2022-07-07 20:05:46 -0300

    commit

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+let ancho;
+let largo;
+let perimetro;
 
+ancho = prompt("Ancho: ");
+ancho = parseInt(ancho);
+
+largo = prompt("Largo: ");
+largo = parseInt(largo);
+
+perimetro = ancho * 2 + largo * 2;
+
+alert("Permimetro: " + perimetro);
 }
