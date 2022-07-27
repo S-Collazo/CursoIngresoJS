c117b09fc94ce6edbf09028f1951aafc503594b2
S-Collazo <santiago_collazo@hotmail.com>
2022-07-26 21:25:56 -0300

    commit

---
+++ PracticaParcial01.js
@@ -0,0 +1,104 @@
+/*
+1.	Se ingresan 5 importes, marca del producto 
+y pais de origen (China, Uruguay o Paraguay).
+Calcular y mostrar:
+a.	Minimo importe con su pais
+b.	Maximo importe con su marca
+c.	Promedio importe
+d.	Cantidad de productos de China
+e.	Sobre el maximo encontrado aplicar un descuento del 10% a dicho importe.
+*/
+
+function mostrar() {
+    let bandera;
+    let i;
+
+    let importe;
+    let marca;
+    let pais;
+
+    let importeTotal;
+    let importeMaximo;
+    let paisImporteMaximo;
+    let marcaImporteMaximo;
+    let importeMinimo;
+    let paisImporteMinimo;
+    let marcaImporteMinimo;
+    let productosChina;
+
+    let promedio;
+    let importeMaximoDesc;
+
+    let mensaje;
+
+    bandera = true;
+
+    importe = 0;
+
+    productosChina = 0;
+
+    importeTotal = 0;
+
+    for (i = 0; i < 5; i++) {
+        importe = prompt("Importe:");
+        importe = parseInt(importe);
+
+        while (isNaN(importe) || importe <= 0) {
+            importe = prompt("El importe no es un númnero. Reingresar importe:");
+            importe = parseInt(importe);
+        }
+
+        marca = prompt("Marca:");
+
+        pais = prompt("País (China, Uruguay o Paraguay):");
+        pais = pais.toLowerCase();
+
+        while (pais != "china" && pais != "uruguay" && pais != "paraguay") {
+            pais = prompt("Pais incorrecto. Reingresar país:");
+            pais = pais.toLowerCase();
+        }
+
+        if (bandera) {
+            importeMaximo = importe;
+            paisImporteMaximo = pais;
+            marcaImporteMaximo = marca;
+
+            importeMinimo = importe;
+            paisImporteMinimo = pais;
+            marcaImporteMinimo = marca;
+
+            bandera = false;
+        } else {
+            if (importe > importeMaximo) {
+                importeMaximo = importe;
+                paisImporteMaximo = pais;
+                marcaImporteMaximo = marca;
+            }
+            if (importe < importeMinimo) {
+                importeMinimo = importe;
+                paisImporteMinimo = pais;
+                marcaImporteMinimo = marca;
+            }
+        }
+
+        if (pais == "china") {
+            productosChina++;
+        }
+
+        importeTotal = importeTotal + importe;
+    }
+
+    promedio = importeTotal / 5;
+
+    importeMaximoDesc = importeMaximo - (importeMaximo * 0.10);
+
+    mensaje = "a.	Minimo importe con su pais: " + importeMinimo + " (" + paisImporteMinimo + ")" + "\n";
+    mensaje += "b.	Maximo importe con su marca: " + importeMaximo + " (" + marcaImporteMaximo + ")" + "\n";
+    mensaje += "c.	Promedio importe: " + promedio + "\n";
+    mensaje += "d.	Cantidad de productos de China: " + productosChina + "\n";
+    mensaje += "e.	Maximo con descuento del 10%: " + importeMaximoDesc + "\n";
+
+    alert(mensaje);
+}
+
+// Santiago Collazo - santiago.collazo7@gmail.com
